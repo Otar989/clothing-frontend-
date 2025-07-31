ed1db18c04ff2464269408cf0392c6f1b93d8d84
Otar989 <otar989@gmail.com>
2025-07-31 23:10:29 +0300

    Delete ss directory

--- ss/modules/localizeDate.js
+++
@@ -1,8 +0,0 @@
-const localizeDate = (date) =>
-  new Date(date * 1000).toLocaleString("ru", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-export default localizeDate;
--- ss/modules/truncateText.js
+++
@@ -1,9 +0,0 @@
-const truncateText = (text, maxLength = 35) => {
-  if (text.length > maxLength) {
-    return text.trim().slice(0, maxLength - 3) + "...";
-  }
-
-  return text;
-};
-
-export default truncateText;
